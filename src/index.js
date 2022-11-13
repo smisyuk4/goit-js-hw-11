79bce0026632def5c438e6270a55cfa6f90adbac
Sergey Misyuk <smisyuk@gmail.com>
2022-11-14 00:35:47 +0200

    import npm's and remove files teml

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css'
+
