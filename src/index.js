dacefa4a22ae07abfaaeffd477735c6a0645f151
Sergey Misyuk <smisyuk@gmail.com>
2022-11-16 00:37:08 +0200

    make some extra tasks

--- src/index.js
+++ src/index.js
@@ -19,7 +19,10 @@ refs.galleryContainer.addEventListener('click', onClickPhoto)
 refs.loadMoreBtn.addEventListener('click', onClickButtonLoadMore)
 
 let searchQuery = ''
+let totalHits = null
 let numberPage = null
+const photoPerPage = 40
+
 
 async function findPhoto(event) {
     event.preventDefault()
@@ -31,21 +34,33 @@ async function findPhoto(event) {
 
     if (searchQuery.length === 0) {
         Notify.failure('No value for search');
-        console.log('No value for search')
         return
     }
     
     try {
         const data = await photoFetch(searchQuery, numberPage)
+        totalHits = data.data.totalHits        
         console.log(data.data)
 
-        if (data.data.hits.length === 0) {
+        if (totalHits === 0) {
             Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-            console.log('Sorry, there are no images matching your search query. Please try again.')
+            clearPage() 
+            searchQuery = ''
         }
 
-        clearPage() 
-        drawMarkup(data) 
+        if (totalHits > photoPerPage) {
+            refs.loadMoreBtn.classList.remove('hidden')
+        }
+
+        if (totalHits < photoPerPage) {
+            refs.loadMoreBtn.classList.add('hidden')
+        }
+
+        if (totalHits > 0) {
+            Notify.success(`Hooray! We found ${totalHits} images.`)
+            clearPage() 
+            drawMarkup(data) 
+        }
     } catch (error) {
         Notify.failure(`${error}`);
     }    
@@ -84,8 +99,14 @@ function clearPage() {
 async function onClickButtonLoadMore(event) {
     event.preventDefault() 
     console.log('load more')
-
+    console.log(totalHits)
+    totalHits -= photoPerPage
     numberPage += 1
+
+    if (totalHits < photoPerPage) {
+        refs.loadMoreBtn.classList.add('hidden')
+        Notify.failure(`We're sorry, but you've reached the end of search results.`);
+    }
 
     try {
         const data = await photoFetch(searchQuery, numberPage)
